03e8afafc89553969d606cc76ea595917f1bccc8
gonsaje <song.jaehyun@gmail.com>
2026-05-14 15:51:17 -0400

    initial commit

---
+++ src/main/java/com/dontsweat/api/DontsweatApiApplication.java
@@ -0,0 +1,13 @@
+package com.dontsweat.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DontsweatApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DontsweatApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dontsweat/api/DontsweatApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dontsweat.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DontsweatApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
